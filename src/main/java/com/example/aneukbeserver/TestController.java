1531ce1eaf833acd4bf076577f4a6f66be19cc55
yeojin <yeojin7010+L@gmail.com>
2024-10-04 16:55:41 +0900

    Update TestController: Github Action test

---
+++ src/main/java/com/example/aneukbeserver/TestController.java
@@ -0,0 +1,16 @@
+package com.example.aneukbeserver;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequestMapping("/")
+@RestController
+public class TestController {
+    @GetMapping
+    public String test() {
+        return "Hello world";
+    }
+}

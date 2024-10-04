9491b22f000dafdf349568a532e706b004c2c3c0
yeojin <yeojin7010+L@gmail.com>
2024-10-04 16:13:41 +0900

    Springboot Initailize

---
+++ src/main/java/com/example/aneukbeserver/ANeukBeServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.aneukbeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ANeukBeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ANeukBeServerApplication.class, args);
+    }
+
+}

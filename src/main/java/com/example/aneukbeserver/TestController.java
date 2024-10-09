54bbf1807abfe7bd46eea65b9158efdf4039dd37
minseok128 <minseok128128@gmail.com>
2024-10-09 13:30:02 +0900

    Update TestController : python api get test

--- src/main/java/com/example/aneukbeserver/TestController.java
+++ src/main/java/com/example/aneukbeserver/TestController.java
@@ -3,6 +3,7 @@ package com.example.aneukbeserver;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -16,6 +17,9 @@ public class TestController {
 
     @GetMapping("/test")
     public List<String> test2() {
-        return List.of("SUCCESS?!?!?!?", "SUCCESS?!?!?!?", "WOW!!!!!?!?!?!?");
+        RestTemplate restTemplate = new RestTemplate();
+        String externalContent = restTemplate.getForObject("http://localhost:2518/test", String.class);
+        System.out.println(externalContent);
+        return List.of("sprint api content", externalContent);
     }
 }

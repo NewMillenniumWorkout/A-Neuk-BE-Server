d939ac47ec7ee02a37c366344d30cc75a53cd6e7
yeojin <yeojin7010+L@gmail.com>
2024-11-11 12:28:24 +0900

    Update Chat: chat type 추가

--- src/main/java/com/example/aneukbeserver/auth/jwt/JwtAuthFilter.java
+++ src/main/java/com/example/aneukbeserver/auth/jwt/JwtAuthFilter.java
@@ -37,7 +37,9 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         // request Header에서 AccessToken을 가져온다
-        String atc = request.getHeader("Authorization").substring(7);
+        String atc = request.getHeader("Authorization");
+
+        if(atc != null) atc = request.getHeader("Authorization").substring(7);
 
         // 토큰 검사 생략 (모두 허용 URL의 경우 토큰 검사 통과)
         if (!StringUtils.hasText(atc)) {

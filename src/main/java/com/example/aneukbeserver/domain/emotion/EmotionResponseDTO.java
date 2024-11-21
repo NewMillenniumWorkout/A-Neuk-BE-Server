e025c8edbeaa36e570bb21785b62a5dfee734d0d
yeojin <yeojin7010+L@gmail.com>
2024-11-21 14:38:31 +0900

    Update Emotion: 감정 정보 api 구현

---
+++ src/main/java/com/example/aneukbeserver/domain/emotion/EmotionResponseDTO.java
@@ -0,0 +1,16 @@
+package com.example.aneukbeserver.domain.emotion;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmotionResponseDTO {
+    private Long emotion_id;
+    private String emotion_name;
+    private String description;
+}
